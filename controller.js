938d1751e421678e184f499c9f6a78c13ddc01ee
Christopher Brown <io@henrian.com>
2018-07-01 21:33:39 -0500

    Delint via eslint ... --fix

--- controller.js
+++ controller.js
@@ -1,66 +1,65 @@
-/*jslint node: true */
-var url = require('url');
+var url = require('url')
 
-var logger = require('loge');
-var redis = require('redis');
+var logger = require('loge')
+var redis = require('redis')
 var request = require('request').defaults({
   headers: {
     'User-Agent': 'github-proxy',
-    'Authorization': 'token ' + process.env.GITHUB_TOKEN
-  }
-});
+    'Authorization': 'token ' + process.env.GITHUB_TOKEN,
+  },
+})
 
 // expires in 6 hours = 6 hr * (60 min / hr) * (60 sec / min)
-var cache_ttl_seconds = 6*60*60;
+var cache_ttl_seconds = 6 * 60 * 60
 
-var get = function(url,  callback) {
+function get(url, callback) {
   /**
   Check if url is cached in redis; if not, go out and get it.
   */
-  var redis_client = redis.createClient();
+  var redis_client = redis.createClient()
 
   var done = function(err, result) {
-    redis_client.quit();
+    redis_client.quit()
     // escape redis's ugly exception-intercepting clutches
     setImmediate(function() {
-      callback(err, result);
-    });
-  };
+      callback(err, result)
+    })
+  }
 
   redis_client.get(url, function(err, cached) {
-    if (err) return done(err);
+    if (err) return done(err)
 
     if (cached) {
-      logger.debug('Found %s in cache', url);
-      return done(null, cached);
+      logger.debug('Found %s in cache', url)
+      return done(null, cached)
     }
 
     request.get(url, function(err, response, body) {
-      if (err) return done(err);
+      if (err) return done(err)
       if (response.statusCode != 200) {
-        logger.info('Non-200 response', body);
+        logger.info('Non-200 response', body)
       }
 
       redis_client.setex(url, cache_ttl_seconds, body, function(err) {
-        if (err) return done(err);
+        if (err) return done(err)
 
-        logger.debug('Retrieved %s from web', url);
-        done(null, body);
-      });
-    });
-  });
-};
+        logger.debug('Retrieved %s from web', url)
+        done(null, body)
+      })
+    })
+  })
+}
 
 module.exports = function(req, res) {
-  var urlObj = url.parse(req.url);
+  var urlObj = url.parse(req.url)
   // replace http://localhost (or whatever) with https://api.github.com
-  urlObj.hostname = 'api.github.com';
-  urlObj.protocol = 'https:';
-  var upstream_url = url.format(urlObj);
+  urlObj.hostname = 'api.github.com'
+  urlObj.protocol = 'https:'
+  var upstream_url = url.format(urlObj)
   get(upstream_url, function(err, body) {
-    if (err) return res.die(err);
+    if (err) return res.die(err)
 
-    res.setHeader('Content-Type', 'application/json');
-    res.end(body);
-  });
-};
+    res.setHeader('Content-Type', 'application/json')
+    res.end(body)
+  })
+}
--- server.js
+++ server.js
@@ -1,48 +1,47 @@
 #!/usr/bin/env node
-/*jslint node: true */
-var http = require('http');
-var util = require('util');
-var logger = require('loge');
+var http = require('http')
+var util = require('util')
+var logger = require('loge')
 
 var optimist = require('optimist')
-  .describe({
-    hostname: 'hostname to listen on',
-    port: 'port to listen on',
+.describe({
+  hostname: 'hostname to listen on',
+  port: 'port to listen on',
 
-    help: 'print this help message',
-    verbose: 'print extra output',
-    version: 'print version',
-  })
-  .boolean(['help', 'verbose', 'version'])
-  .alias({verbose: 'v'})
-  .default({
-    hostname: '127.0.0.1',
-    port: 1443,
-  });
+  help: 'print this help message',
+  verbose: 'print extra output',
+  version: 'print version',
+})
+.boolean(['help', 'verbose', 'version'])
+.alias({verbose: 'v'})
+.default({
+  hostname: '127.0.0.1',
+  port: 1443,
+})
 
-var argv = optimist.argv;
-logger.level = argv.verbose ? 'debug' : 'info';
+var argv = optimist.argv
+logger.level = argv.verbose ? 'debug' : 'info'
 
 if (argv.help) {
-  optimist.showHelp();
+  optimist.showHelp()
 }
 else if (argv.version) {
-  console.log(require('./package').version);
+  console.log(require('./package').version)
 }
 else {
-  var controller = require('./controller');
+  var controller = require('./controller')
   http.createServer(function(req, res) {
-    var req_id = util.format('%s %s', req.method, req.url);
-    console.time(req_id);
+    var req_id = util.format('%s %s', req.method, req.url)
+    console.time(req_id)
     req.on('end', function() {
-      console.timeEnd(req_id);
-    });
+      console.timeEnd(req_id)
+    })
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*')
     // res.setHeader('Access-Control-Allow-Headers', 'X-Args');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    controller(req, res);
+    res.setHeader('Access-Control-Allow-Methods', '*')
+    controller(req, res)
   }).listen(argv.port, argv.hostname, function() {
-    logger.info('listening on http://%s:%d', argv.hostname, argv.port);
-  });
+    logger.info('listening on http://%s:%d', argv.hostname, argv.port)
+  })
 }
