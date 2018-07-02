aa7da312c1da181083bed9b26cb53fec4c783fcb
Christopher Brown <io@henrian.com>
2018-07-01 21:59:05 -0500

    Modernize & refactor stats example

--- examples/stats.js
+++ examples/stats.js
@@ -1,7 +1,32 @@
-var github_proxy_hostname = 'localhost'
+const github_proxy_hostname = 'localhost'
+
+/**
+Create a new Element with the given attributes and childNodes
+*/
+function h(tagName, attrs, ...childNodes) {
+  const el = document.createElement(tagName)
+  // treat attrs as the first childNode if it looks like a childNode
+  if (attrs instanceof Element || typeof attrs == 'string') {
+    childNodes.unshift(attrs)
+  }
+  else {
+    Object.keys(attrs).forEach(key => {
+      el.setAttribute(key, attrs[key])
+    })
+  }
+  childNodes.forEach(childNode => {
+    if (childNode instanceof Element) {
+      el.appendChild(childNode)
+    }
+    else {
+      el.appendChild(document.createTextNode(childNode))
+    }
+  })
+  return el
+}
 
 function getJSON(url, callback) {
-  var xhr = new XMLHttpRequest()
+  const xhr = new XMLHttpRequest()
   xhr.open('GET', url)
   xhr.responseType = 'json'
   xhr.send()
@@ -32,18 +57,18 @@ getStats('joyent', 'node', (err, stats) => {
 function getStats(owner, repo, callback) {
   getJSON('https://' + github_proxy_hostname + '/repos/' + owner + '/' + repo, (err, result) => {
     if (err) return callback(err)
-
+    const {forks_count, stargazers_count, subscribers_count} = result
     callback(null, {
-      forks: result.forks_count,
-      stars: result.stargazers_count,
-      subscribers: result.subscribers_count,
+      forks: forks_count,
+      stars: stargazers_count,
+      subscribers: subscribers_count,
     })
   })
 }
 
 function parseOwnerRepo(anchor) {
   // maybe better as one big regex?
-  var path_segments = anchor.pathname.split('/')
+  const path_segments = anchor.pathname.split('/')
   if (anchor.hostname == 'github.com') {
     // e.g., ["", "substack", "node-browserify"]
     if (path_segments.length >= 3) {
@@ -51,7 +76,7 @@ function parseOwnerRepo(anchor) {
     }
   }
 
-  var gh_pages_match = anchor.hostname.match(/(.+)\.github\.(io|com)/)
+  const gh_pages_match = anchor.hostname.match(/(.+)\.github\.(io|com)/)
   if (gh_pages_match) {
     // e.g., ["", "Pygmy", "Docs", "index.html"]
     if (path_segments.length >= 2) {
@@ -61,37 +86,25 @@ function parseOwnerRepo(anchor) {
 }
 
 function decorateAnchorWithStats(anchor, stats) {
-  // var stars = document.createElement('sup');
-  // stars.textContent = stats.stars + ' ★';
-  // anchor.appendChild(stars);
-
-  // var forks = document.createElement('sub');
-  // forks.textContent = stats.forks + ' ⑂';
-  // anchor.appendChild(forks);
-  var table = document.createElement('table')
+  // anchor.appendChild(h('sup', stats.stars + ' ★'));
+  // anchor.appendChild(h('sub', stats.forks + ' ⑂'));
+  const table = h('table',
+                  h('tr',
+                    h('td', stats.stars),
+                    h('td', '★')),
+                  h('tr',
+                    h('td', stats.forks),
+                    h('td', '⑂')))
   table.classList.add('stats')
-
-  var star_tr = table.appendChild(document.createElement('tr'))
-  var star_td1 = star_tr.appendChild(document.createElement('td'))
-  star_td1.textContent = stats.stars
-  var star_td2 = star_tr.appendChild(document.createElement('td'))
-  star_td2.textContent = '★'
-
-  var fork_tr = table.appendChild(document.createElement('tr'))
-  var fork_td1 = fork_tr.appendChild(document.createElement('td'))
-  fork_td1.textContent = stats.forks
-  var fork_td2 = fork_tr.appendChild(document.createElement('td'))
-  fork_td2.textContent = '⑂'
-
   anchor.appendChild(table)
 }
 
 function decorate(container) {
-  var anchors = container.getElementsByTagName('a')
+  const anchors = container.getElementsByTagName('a')
   function loop(i) {
-    var anchor = anchors[i]
+    const anchor = anchors[i]
     if (anchor) {
-      var owner_repo = parseOwnerRepo(anchor)
+      const owner_repo = parseOwnerRepo(anchor)
       if (owner_repo) {
         getStats(owner_repo.owner, owner_repo.repo, (err, stats) => {
           if (err) {
